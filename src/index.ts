b46e12524a324936373d9e59cf315aeb4767a674
Dimitris Stavroulakis <jimstav@gmail.com>
2025-04-13 18:37:54 +0300

    Proof of Concept

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import express from 'express';
-import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import express from 'express';
+import { router as controllerRouter } from './controllers/decorators/controller';
+import './controllers/LoginController';
+import { router } from './routes/loginRoutes';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['session'] }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
