dd732f14a5feca3d999322a104aa04f9c9e2dfdb
Dimitris Stavroulakis <jimstav@gmail.com>
2025-04-13 21:19:25 +0300

    Body Validators

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -2,6 +2,11 @@ import 'reflect-metadata';
 import { AppRouter } from '../../AppRouter';
 import { MetadataKeys } from './MetadataKeys';
 import { Methods } from './Methods';
+import { NextFunction, RequestHandler } from 'express';
+
+// function bodyValidators(keys: string): RequestHandler {
+//   return function (req: Request, res: Response, next: NextFunction) {};
+// }
 
 export function controller(routePrefix: string) {
   return function (target: Function) {
