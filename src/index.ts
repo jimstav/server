0a67850b895118d2bb6f298e89229555ca6295c8
Dimitris Stavroulakis <jimstav@gmail.com>
2025-04-13 19:41:04 +0300

    A Few Fixups

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 import express from 'express';
-import { router as controllerRouter } from './controllers/decorators/controller';
+import { AppRouter } from './AppRouter';
 import './controllers/LoginController';
 import { router } from './routes/loginRoutes';
 
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['session'] }));
 app.use(router);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
