f2968b33e487589ab23df6f1cc7a6e3dcae7ada9
Dimitris Stavroulakis <jimstav@gmail.com>
2025-04-14 00:56:20 +0300

    Fixing Routes

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,12 @@ import cookieSession from 'cookie-session';
 import express from 'express';
 import { AppRouter } from './AppRouter';
 import './controllers/LoginController';
-import { router } from './routes/loginRoutes';
+import './controllers/RootController';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['session'] }));
-app.use(router);
 app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
